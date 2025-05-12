a6d7c89a8f4b40cf2e52b59cea4ed2bcb98277e6
Davi Soares <davisrmcd@gmail.com>
2025-05-12 20:46:25 -0300

    Update gerar_histo_imagemPB.py

--- equalização/python/gerar_histo_imagemPB.py
+++ equalização/python/gerar_histo_imagemPB.py
@@ -1,14 +1,14 @@
 from collections import Counter
 from PIL import Image
 
-# Abre a imagem JPG e converte para tons de cinza
+
 img = Image.open('Imagem_PB.jpg').convert('L')
 pixels = list(img.getdata())
 
-# Conta as ocorrências de cada valor de pixel (0 a 255)
+
 contagem = Counter(pixels)
 
-# Salva o histograma em um arquivo txt
+
 with open('histograma_imagem_alto_nivel.txt', 'w', encoding='utf-8') as f:
     for valor in range(256):
         f.write(f"Pixel {valor} - ocorrencia {contagem.get(valor, 0)}\n")
