27756b381160822b912ba87a110817f175b073f1
Davi Soares <davisrmcd@gmail.com>
2025-05-12 20:46:39 -0300

    Update converter_para_raw_rgb.py

--- histograma/python/converter_para_raw_rgb.py
+++ histograma/python/converter_para_raw_rgb.py
@@ -1,8 +1,8 @@
 from PIL import Image
 
-# Altere o nome da imagem se necessário
+
 img = Image.open('imagem.jpg').convert('RGB').resize((176, 144))
 
-# Salva o RAW RGB (cada pixel = 3 bytes: R, G, B)
+
 with open('img_rgb.raw', 'wb') as f:
     f.write(img.tobytes())
