d359a093be4ae8603e45490292a237704a00e677
Davi Soares <davisrmcd@gmail.com>
2025-05-12 20:46:35 -0300

    Update converter_para_raw.py

--- histograma/python/converter_para_raw.py
+++ histograma/python/converter_para_raw.py
@@ -1,12 +1,11 @@
 from PIL import Image
 import numpy as np
 
-# Abra a imagem
+
 img = Image.open('imagem.jpg').convert('L')  # 'L' = grayscale
 
-# Redimensiona para 176x144
+
 img = img.resize((176, 144))
 
-# Salva como RAW (apenas os bytes dos pixels)
 with open('img_raw.y', 'wb') as f:
     f.write(img.tobytes())
